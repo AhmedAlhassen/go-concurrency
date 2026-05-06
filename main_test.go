781b0cf2fd9ffcb8214c3c8b40bd1766c371bb3a
ahmed-alhassen <aahmed@iskan.abudhabi>
2026-05-06 12:34:47 +0400

    Implement WebSocket client communication and server message handling

--- main_test.go
+++ main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"sync"
 	"testing"
 	"time"
@@ -19,34 +18,53 @@ type TestConfig struct {
 	wg          *sync.WaitGroup
 }
 
-func DialServer(wg *sync.WaitGroup) {
+func DialServer(clientNum int, wg *sync.WaitGroup, failedAt chan<- int) {
 	defer wg.Done()
+
 	// client
 	dialer := websocket.DefaultDialer
 
 	conn, _, err := dialer.Dial(fmt.Sprintf("%s%s", host, WSPort), nil)
 	if err != nil {
-		log.Fatal(err)
+		select {
+		case failedAt <- clientNum:
+			fmt.Printf("first failed client was #%d: %v\n", clientNum, err)
+		default:
+		}
+		return
 	}
-	fmt.Println("connected to the server", conn.LocalAddr().String())
 
-	time.Sleep(1 * time.Second)
-	conn.Close()
+	defer func() {
+		conn.Close()
+	}()
+	fmt.Printf("client #%d connected to the server %s\n", clientNum, conn.LocalAddr().String())
+
+	time.Sleep(2 * time.Second)
+
 }
 
 func Test_Connection(t *testing.T) {
 	// s := NewServer()
 	go createWSServer()
+	time.Sleep(1 * time.Second)
+
 	tc := TestConfig{
-		clientCount: 10,
+		clientCount: 5,
 		wg:          new(sync.WaitGroup),
 	}
 	tc.wg.Add(tc.clientCount)
+	failedAt := make(chan int, 1)
 
-	for range tc.clientCount {
-		go DialServer(tc.wg)
+	for i := range tc.clientCount {
+		go DialServer(i+1, tc.wg, failedAt)
 	}
 
 	tc.wg.Wait()
+	select {
+	case n := <-failedAt:
+		t.Fatalf("first client failure happened at client #%d", n)
+	default:
+	}
+	time.Sleep(1 * time.Second)
 	fmt.Println("Exiting test....")
 }
